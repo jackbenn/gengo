c76a1c5bf38ea5083426f0358dba5a45749c42ae
jackbenn <jack@bennetto.com>
2019-01-22 22:58:08 -0800

    add rule options to web app (partial)

--- gengo/app/routes.py
+++ gengo/app/routes.py
@@ -19,8 +19,14 @@ def join():
 def game():
     game_name = request.args.get('game_name')
     board_size = int(request.args.get('board_size'))
+    allow_suicide = "allow_suicide" in request.args
+    play_black = "play_black" in request.args
+    handicap = int(request.args.get('handicap'))
     action = request.args.get('action')
     return render_template('game.html',
                            game_name=game_name,
                            board_size=board_size,
+                           allow_suicide=allow_suicide,
+                           play_black=play_black,
+                           handicap=handicap,
                            action=action)
